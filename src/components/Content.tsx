6a4d59bb9c9cdc8aae1947f83a6e314829bb3ecc
Daniel <daniel.mileski@cidadania.gov.br>
2021-09-16 00:00:24 -0300

    :tada: Desafio completo!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+import { MovieCard } from "./MovieCard";
+
+import { MovieProps } from "../interfaces/movie";
+import { GenreResponseProps } from "../interfaces/genre";
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[]
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
